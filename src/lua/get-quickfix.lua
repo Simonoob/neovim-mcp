3bfe56fcd39d182523263f0e55d035c472982513
simonoob <simonefiorentini1@gmail.com>
2026-04-10 23:49:35 +0300

    chore(formatting)

--- src/lua/get-quickfix.lua
+++ src/lua/get-quickfix.lua
@@ -1,16 +1,21 @@
-local qf = vim.fn.getqflist({ all = 1 })
-local items = {}
-for _, item in ipairs(qf.items or {}) do
-  local fname = ""
+local quickfix_list = vim.fn.getqflist({ all = 1 })
+
+local formatted_items = {}
+
+for _, item in ipairs(quickfix_list.items or {}) do
+  local filename = ""
+
   if item.bufnr and item.bufnr > 0 then
-    fname = vim.api.nvim_buf_get_name(item.bufnr)
+    filename = vim.api.nvim_buf_get_name(item.bufnr)
   end
-  table.insert(items, {
-    file = fname,
+
+  table.insert(formatted_items, {
+    file = filename,
     line = item.lnum or 0,
     col = item.col or 0,
     text = item.text or "",
     type = item.type or "",
   })
 end
-return { title = qf.title or "", items = items }
+
+return { title = quickfix_list.title or "", items = formatted_items }
