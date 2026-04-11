12fc3007df47cec5a8f33142ed17b50c468df178
simonoob <simonefiorentini1@gmail.com>
2026-04-11 09:40:30 +0300

    chore(search-symbols): simplify and use async

--- src/lua/search-symbols.lua
+++ src/lua/search-symbols.lua
@@ -1,12 +1,12 @@
-local query = select(1, ...)
-local bufnr = vim.api.nvim_get_current_buf()
-if #vim.lsp.get_clients({ bufnr = bufnr }) == 0 then
-  return { error = "No LSP client attached to current buffer" }
-end
+local query = ...
 
-local results = vim.lsp.buf_request_sync(bufnr, "workspace/symbol", { query = query }, 5000)
-if not results then
-  return { error = "LSP request timed out" }
+-- buffer should be already opened, no need to load it
+local bufnr = vim.api.nvim_get_current_buf()
+
+if not vim.wait(250, function()
+  return #vim.lsp.get_clients({ bufnr = bufnr }) > 0
+end) then
+  return { error = "No LSP client attached to current buffer" }
 end
 
 local kind_map = {
@@ -38,22 +38,38 @@ local kind_map = {
   [26] = "TypeParameter",
 }
 
-local symbols = {}
-for _, res in pairs(results) do
-  if res.result then
-    for _, sym in ipairs(res.result) do
-      if #symbols >= 50 then
-        break
+local process_and_return_results = function(results)
+  local symbols = {}
+  for _, res in pairs(results) do
+    if res.result then
+      for _, sym in ipairs(res.result) do
+        if #symbols >= 50 then
+          break
+        end
+        local loc = sym.location
+        table.insert(symbols, {
+          name = sym.name,
+          kind = kind_map[sym.kind] or "Unknown",
+          file = vim.uri_to_fname(loc.uri),
+          line = loc.range.start.line + 1,
+          col = loc.range.start.character + 1,
+        })
       end
-      local loc = sym.location
-      table.insert(symbols, {
-        name = sym.name,
-        kind = kind_map[sym.kind] or "Unknown",
-        file = vim.uri_to_fname(loc.uri),
-        line = loc.range.start.line + 1,
-        col = loc.range.start.character + 1,
-      })
     end
   end
+  return symbols
 end
-return symbols
+
+local results = nil
+
+vim.lsp.buf_request_all(bufnr, "workspace/symbol", { query = query }, function(res)
+  results = process_and_return_results(res)
+end)
+
+if not vim.wait(500, function()
+  return results ~= nil
+end) then
+  return { error = "LSP workspace symbols request timed out" }
+end
+
+return results
